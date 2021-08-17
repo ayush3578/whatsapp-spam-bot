dc816b1c8dd309e22ead4ccedfa812c761dad561
ayush3578 <84192387+ayush3578@users.noreply.github.com>
2021-08-17 12:45:24 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,13 @@
+import pyautogui 
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com")
+time.sleep(10)
+#select the time you want before the code starts spamming .just change the number
+while True:
+    pyautogui.press("h")
+    pyautogui.press("e")
+    pyautogui.press("l")
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("enter")
