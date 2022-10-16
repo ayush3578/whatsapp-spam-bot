63c6c494b95e8442bc7dab5010da877c9fbb665e
UNKNOWN <84192387+ayush3578@users.noreply.github.com>
2022-10-16 21:44:06 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -6,8 +6,5 @@ time.sleep(10)
 #select the time you want before the code starts spamming .just change the number
 while True:
     pyautogui.press("h")
-    pyautogui.press("e")
-    pyautogui.press("l")
-    pyautogui.press("l")
-    pyautogui.press("o")
+    pyautogui.press("i)
     pyautogui.press("enter")
